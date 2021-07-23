441eaf5575fc7e893d7c67b79a3d1e8c9f8bfe0a
Zvereck20 <kerusov20@gmail.com>
2021-07-23 17:16:56 +1000

    Finish

--- build/js/script.js
+++ build/js/script.js
@@ -1,45 +1,98 @@
-// Navigation block
+// Modal window
 
-const body = document.querySelector('main')
-const header = document.querySelector('header');
-const navMain = document.querySelector('.navigation');
-const navToggle = document.querySelector('.navigation__toggle');
+const body = document.querySelector('body')
+const modal = document.querySelector('.modal');
+const closeModalButton = modal.querySelector('.modal__toggle')
+const openModalButton = document.querySelector('.navigation__button');
 
-navMain.classList.remove('navigation--nojs');
-header.classList.remove('header--nojs')
+const MODAL_QUESTION = document.querySelector('#modal-question')
+const MODAL_NAME = modal.querySelector('#modal-name');
+const MODAL_PHONE = modal.querySelector('#modal-telephone');
 
-navToggle.addEventListener('click', function () {
+function existVerticalScroll() {
+  return document.body.offsetHeight > window.innerHeight
+}
 
-  if (navMain.classList.contains('navigation--closed')) {
-    navMain.classList.remove('navigation--closed');
-    navMain.classList.add('navigation--opened');
+function getBodyScrollTop() {
+  return self.pageYOffset || (document.documentElement && document.documentElement.ScrollTop) || (document.body && document.body.scrollTop);
+}
+
+openModalButton.addEventListener('click', e => {
+  e.preventDefault()
+
+  body.dataset.scrollY = getBodyScrollTop()
+
+  modal.classList.remove('visually-hidden')
+
+  modal.querySelector('#modal-name').focus()
+
+  if (existVerticalScroll()) {
     body.classList.add('body-lock')
-  } else {
-    navMain.classList.add('navigation--closed');
-    navMain.classList.remove('navigation--opened');
-    body.classList.remove('body-lock')
+    body.style.top = `-${body.dataset.scrollY}px`
   }
-});
+})
+
+closeModalButton.addEventListener('click', e => {
+  e.preventDefault()
+
+  modal.classList.add('visually-hidden')
+
+  if (existVerticalScroll()) {
+    body.classList.remove('body-lock')
+    window.scrollTo(0, body.dataset.scrollY)
+  }
+})
 
 document.addEventListener('keydown', (evt) => {
   if (evt.keyCode === 27) {
-    if (!navMain.classList.contains('navigation--closed')) {
-      navMain.classList.remove('navigation--opened');
-      navMain.classList.add('navigation--closed');
+    if (!modal.classList.contains('visually-hidden')) {
+      modal.classList.add('visually-hidden');
+    }
+
+    if (existVerticalScroll()) {
       body.classList.remove('body-lock')
+      window.scrollTo(0, body.dataset.scrollY)
     }
   }
 });
 
-const navLinks = document.querySelectorAll('.navigation__link');
+// Local save
 
-navLinks.forEach((link) => {
-  link.addEventListener('click', () => {
-    navMain.classList.add('navigation--closed');
-    navMain.classList.remove('navigation--opened');
+modal.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+
+  localStorage.setItem('modal__name', MODAL_NAME.value);
+  localStorage.setItem('modal__phone', MODAL_PHONE.value);
+  localStorage.setItem('modal__question', MODAL_QUESTION.value);
+  MODAL_NAME.value = '';
+  MODAL_PHONE.value = '';
+  MODAL_QUESTION.value = 'Ваш вопрос';
+
+  modal.classList.add('visually-hidden');
+
+  if (existVerticalScroll()) {
     body.classList.remove('body-lock')
-  })
-})
+    window.scrollTo(0, body.dataset.scrollY)
+  }
+});
+
+const feedbackForm = document.querySelector('.fedback__field');
+const FEEDBACK_NAME = feedbackForm.querySelector('#name')
+const FEEDBACK_PHONE = feedbackForm.querySelector('#telephone')
+const FEEDBACK_QUESTION = feedbackForm.querySelector('#question')
+
+feedbackForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+
+  localStorage.setItem('feedback__name', FEEDBACK_NAME.value);
+  localStorage.setItem('feedback__phone', FEEDBACK_PHONE.value);
+  localStorage.setItem('feedback__question', FEEDBACK_QUESTION.value);
+  FEEDBACK_NAME.value = '';
+  FEEDBACK_PHONE.value = '';
+  FEEDBACK_QUESTION.value = 'Ваш вопрос';
+});
+
+// Form validation
 
 window.addEventListener("DOMContentLoaded", function () {
 
@@ -77,9 +130,45 @@ window.addEventListener("DOMContentLoaded", function () {
     } else setCursorPosition(this.value.length, this)
   };
 
-  var input = document.querySelector("#telephone");
-  input.addEventListener("input", mask, false);
-  input.addEventListener("focus", mask, false);
-  input.addEventListener("blur", mask, false);
-
+  const fields = document.querySelectorAll('input[type="tel"]');
+  fields.forEach((input) => {
+    input.addEventListener("input", mask, false);
+    input.addEventListener("focus", mask, false);
+    input.addEventListener("blur", mask, false);
+  })
 });
+
+
+// Accordion
+
+const accordion = document.querySelectorAll('.accordion');
+const accordionToggle = document.querySelectorAll('.toggle');
+
+accordion.forEach((item) => {
+  item.classList.add('accordion--close');
+})
+
+accordionToggle.forEach((item) => {
+  item.classList.add('toggle--close');
+})
+
+accordionToggle.forEach((toggle) => {
+  toggle.addEventListener('click', () => {
+    const parent = toggle.parentNode;
+
+    if (parent.classList.contains('accordion--close')) {
+      toggle.classList.remove('toggle--close');
+      toggle.classList.add('toggle--open');
+    } else {
+      toggle.classList.add('toggle--close');
+      toggle.classList.remove('toggle--open');   }
+
+
+    if (parent.classList.contains('accordion--close')) {
+      accordion.forEach((item) => item.classList.add('accordion--close'));
+      parent.classList.remove('accordion--close');
+    } else {
+      parent.classList.add('accordion--close');
+    }
+  })
+})
