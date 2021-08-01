6c51ecd36e2e49e0905434be4f93fd25830cafbd
Zvereck20 <kerusov20@gmail.com>
2021-08-01 15:40:31 +1000

    Fixed bugs

--- source/js/script.js
+++ source/js/script.js
@@ -1,46 +1,46 @@
 // Modal window
 
-const body = document.querySelector('body')
+const body = document.querySelector('body');
 const modal = document.querySelector('.modal');
-const closeModalButton = modal.querySelector('.modal__toggle')
+const closeModalButton = modal.querySelector('.modal__toggle');
 const openModalButton = document.querySelector('.navigation__button');
 
-const MODAL_QUESTION = document.querySelector('#modal-question')
+const MODAL_QUESTION = document.querySelector('#modal-question');
 const MODAL_NAME = modal.querySelector('#modal-name');
 const MODAL_PHONE = modal.querySelector('#modal-telephone');
 
 function existVerticalScroll() {
   return document.body.offsetHeight > window.innerHeight
-}
+};
 
 function getBodyScrollTop() {
   return self.pageYOffset || (document.documentElement && document.documentElement.ScrollTop) || (document.body && document.body.scrollTop);
-}
+};
 
 openModalButton.addEventListener('click', e => {
-  e.preventDefault()
+  e.preventDefault();
 
-  body.dataset.scrollY = getBodyScrollTop()
+  body.dataset.scrollY = getBodyScrollTop();
 
-  modal.classList.remove('visually-hidden')
+  modal.classList.remove('visually-hidden');
 
-  modal.querySelector('#modal-name').focus()
+  modal.querySelector('#modal-name').focus();
 
   if (existVerticalScroll()) {
     body.classList.add('body-lock')
     body.style.top = `-${body.dataset.scrollY}px`
-  }
+  };
 })
 
 closeModalButton.addEventListener('click', e => {
-  e.preventDefault()
+  e.preventDefault();
 
-  modal.classList.add('visually-hidden')
+  modal.classList.add('visually-hidden');
 
   if (existVerticalScroll()) {
     body.classList.remove('body-lock')
     window.scrollTo(0, body.dataset.scrollY)
-  }
+  };
 })
 
 document.addEventListener('keydown', (evt) => {
@@ -53,7 +53,7 @@ document.addEventListener('keydown', (evt) => {
       body.classList.remove('body-lock')
       window.scrollTo(0, body.dataset.scrollY)
     }
-  }
+  };
 });
 
 // Local save
@@ -73,13 +73,13 @@ modal.addEventListener('submit', (evt) => {
   if (existVerticalScroll()) {
     body.classList.remove('body-lock')
     window.scrollTo(0, body.dataset.scrollY)
-  }
+  };
 });
 
 const feedbackForm = document.querySelector('.feedback__field');
-const FEEDBACK_NAME = feedbackForm.querySelector('#name')
-const FEEDBACK_PHONE = feedbackForm.querySelector('#telephone')
-const FEEDBACK_QUESTION = feedbackForm.querySelector('#question')
+const FEEDBACK_NAME = feedbackForm.querySelector('#name');
+const FEEDBACK_PHONE = feedbackForm.querySelector('#telephone');
+const FEEDBACK_QUESTION = feedbackForm.querySelector('#question');
 
 feedbackForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
@@ -106,7 +106,7 @@ window.addEventListener("DOMContentLoaded", function () {
       range.moveStart("character", pos);
       range.select()
     }
-  }
+  };
 
   function mask(event) {
     var matrix = "+7 (___) ___ ____",
@@ -135,7 +135,7 @@ window.addEventListener("DOMContentLoaded", function () {
     input.addEventListener("input", mask, false);
     input.addEventListener("focus", mask, false);
     input.addEventListener("blur", mask, false);
-  })
+  });
 });
 
 
@@ -146,14 +146,21 @@ const accordionToggle = document.querySelectorAll('.toggle');
 
 accordion.forEach((item) => {
   item.classList.add('accordion--close');
-})
+});
 
-accordionToggle.forEach((item) => {
-  item.classList.add('toggle--close');
-})
+const emergenceToggle = () => {
+  accordionToggle.forEach((toggle) => {
+    toggle.classList.add('toggle--close');
+  })
+};
+
+emergenceToggle();
 
 accordionToggle.forEach((toggle) => {
   toggle.addEventListener('click', () => {
+
+    emergenceToggle();
+
     const parent = toggle.parentNode;
 
     if (parent.classList.contains('accordion--close')) {
@@ -161,8 +168,8 @@ accordionToggle.forEach((toggle) => {
       toggle.classList.add('toggle--open');
     } else {
       toggle.classList.add('toggle--close');
-      toggle.classList.remove('toggle--open');   }
-
+      toggle.classList.remove('toggle--open');
+    }
 
     if (parent.classList.contains('accordion--close')) {
       accordion.forEach((item) => item.classList.add('accordion--close'));
@@ -171,4 +178,4 @@ accordionToggle.forEach((toggle) => {
       parent.classList.add('accordion--close');
     }
   })
-})
+});
